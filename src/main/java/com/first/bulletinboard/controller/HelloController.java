3f114e7a33045dfeeddb5fc6f048424ecdf66043
sanghee <wjdtkdgml7352@daum.net>
2022-12-20 11:22:14 +0900

    [추가] swaggerConfiguration 1. [build] springfox:swagger-ui & starter 2. [application.yml] swagger version 3. swagger configuration

--- src/main/java/com/first/bulletinboard/controller/HelloController.java
+++ src/main/java/com/first/bulletinboard/controller/HelloController.java
@@ -1,13 +1,16 @@
 package com.first.bulletinboard.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+@Api(tags = {"Test"})
 @RestController
 @RequestMapping("/api/v1/hello")
 public class HelloController {
+    @ApiOperation(value = "hello", notes = "hello 출력")
     @GetMapping
     public ResponseEntity<String> hello() {
         return ResponseEntity.ok().body("hello");
