57560643291229b6aa45fc4c2b9afc30d0bc7860
sanghee <wjdtkdgml7352@daum.net>
2022-12-20 20:39:00 +0900

    [수정] bye -> hello

--- src/main/java/com/first/bulletinboard/controller/HelloController.java
+++ src/main/java/com/first/bulletinboard/controller/HelloController.java
@@ -2,17 +2,20 @@ package com.first.bulletinboard.controller;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 @Api(tags = {"Test"})
 @RestController
+@Slf4j
 @RequestMapping("/api/v1")
 public class HelloController {
     @ApiOperation(value = "hello", notes = "hello 출력")
     @GetMapping("/hello")
     public ResponseEntity<String> hello() {
-        return ResponseEntity.ok().body("bye");
+//        log.info("crontab 실행여부 확인");
+        return ResponseEntity.ok().body("hello");
     }
 }
