9c291146bb5e7718f99745615161191a743a926c
sanghee <wjdtkdgml7352@daum.net>
2022-12-20 09:48:13 +0900

    First Commit

---
+++ src/test/java/com/first/bulletinboard/FirstmissionBulletinBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.first.bulletinboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstmissionBulletinBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
