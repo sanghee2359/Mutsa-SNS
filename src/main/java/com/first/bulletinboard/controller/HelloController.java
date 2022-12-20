f0d15fcd9e3916365d89031d3b707021e518897a
sanghee <wjdtkdgml7352@daum.net>
2022-12-20 16:56:50 +0900

    [수정] hello api

--- src/main/java/com/first/bulletinboard/controller/HelloController.java
+++ src/main/java/com/first/bulletinboard/controller/HelloController.java
@@ -8,10 +8,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 @Api(tags = {"Test"})
 @RestController
-@RequestMapping("/api/v1/hello")
+@RequestMapping("/api/v1")
 public class HelloController {
     @ApiOperation(value = "hello", notes = "hello 출력")
-    @GetMapping
+    @GetMapping("/hello")
     public ResponseEntity<String> hello() {
         return ResponseEntity.ok().body("hello");
     }
