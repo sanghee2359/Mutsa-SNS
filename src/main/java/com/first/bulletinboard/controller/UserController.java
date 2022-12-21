87f781052b9f97ed1c2104073208cc207ef5119a
sanghee <wjdtkdgml7352@daum.net>
2022-12-22 03:35:45 +0900

    [Update] User API에 user/을 추가

--- src/main/java/com/first/bulletinboard/controller/UserController.java
+++ src/main/java/com/first/bulletinboard/controller/UserController.java
@@ -4,6 +4,7 @@ import com.first.bulletinboard.domain.Response;
 import com.first.bulletinboard.domain.dto.*;
 import com.first.bulletinboard.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,13 +12,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v1/users")
 public class UserController {
+    @Autowired
     private final UserService userService;
     @PostMapping("/join")
     public Response<UserJoinResponse> join(@RequestBody UserJoinRequest userJoinRequest) {
         UserDto userDto = userService.join(userJoinRequest);
-        return Response.success(new UserJoinResponse(userDto.getUserName(),userDto.getPassword()));
+        return Response.success(new UserJoinResponse(userDto.getId(), userDto.getUserName()));
     }
     @PostMapping("/login")
     public Response<UserLoginResponse> login(@RequestBody UserLoginRequest userLoginRequest) {
