fbf80a1a1192cdee0429c703d1a9f992d8b0e080
sanghee <wjdtkdgml7352@daum.net>
2022-12-23 11:36:10 +0900

    [Add] Token이 없으면 권한 없음 & Token .isExpired() 만료 확인

--- src/main/java/com/first/bulletinboard/filter/JwtTokenFilter.java
+++ src/main/java/com/first/bulletinboard/filter/JwtTokenFilter.java
@@ -1,7 +1,10 @@
 package com.first.bulletinboard.filter;
 
 import com.first.bulletinboard.service.UserService;
+import com.first.bulletinboard.utils.JwtTokenUtil;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -15,14 +18,33 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 @RequiredArgsConstructor
+@Slf4j
 public class JwtTokenFilter extends OncePerRequestFilter {
 
     private final UserService userService;
     private final String secretKey;
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        final String authorization = request.getHeader(HttpHeaders.AUTHORIZATION);
+
+
+        if(authorization == null || !authorization.startsWith("Bearar ")) {
+            log.info("authorization을 잘못 보냈습니다.");
+            filterChain.doFilter(request, response);
+            return;
+        }
+        // userName에서 token꺼내기
+        String token = authorization.split(" ")[1];
+        if(JwtTokenUtil.isExpired(token, secretKey)) {
+            log.info("token이 만료되었습니다.");
+            filterChain.doFilter(request, response);
+            return;
+        }
+
         // username 꺼내기
         String userName = "";
+
         UsernamePasswordAuthenticationToken authenticationToken =
                 new UsernamePasswordAuthenticationToken(userName,null, List.of(new SimpleGrantedAuthority("USER")));
         // detail 추가
--- src/main/java/com/first/bulletinboard/utils/JwtTokenUtil.java
+++ src/main/java/com/first/bulletinboard/utils/JwtTokenUtil.java
@@ -7,6 +7,10 @@ import io.jsonwebtoken.SignatureAlgorithm;
 import java.util.Date;
 
 public class JwtTokenUtil {
+    public static boolean isExpired(String token, String secretKey) {
+        return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token)
+                .getBody().getExpiration().before(new Date());
+    }
     public static String createToken(String userName, String key, long expireTimeMs) {
         Claims claims = Jwts.claims();
         claims.put("userName", userName);
