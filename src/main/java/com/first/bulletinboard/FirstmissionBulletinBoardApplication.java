db6696433a7411385fc986508a972c06e2362d39
sanghee <wjdtkdgml7352@daum.net>
2022-12-27 14:30:06 +0900

    [Add] Http에서 PutMapping, DeletMapping 사용

--- src/main/java/com/first/bulletinboard/FirstmissionBulletinBoardApplication.java
+++ src/main/java/com/first/bulletinboard/FirstmissionBulletinBoardApplication.java
@@ -2,13 +2,21 @@ package com.first.bulletinboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
+
 @EnableJpaAuditing
 @SpringBootApplication
 public class FirstmissionBulletinBoardApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FirstmissionBulletinBoardApplication.class, args);
+	}
+	// 이 부분 추가
+	@Bean
+	public HiddenHttpMethodFilter hiddenHttpMethodFilter(){
+		return new HiddenHttpMethodFilter();
 	}
 
 }
--- src/main/java/com/first/bulletinboard/domain/entity/UserRole.java
+++ src/main/java/com/first/bulletinboard/domain/entity/UserRole.java
@@ -1,4 +1,4 @@
-package com.first.bulletinboard.domain.dto.user;
+package com.first.bulletinboard.domain.entity;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 
