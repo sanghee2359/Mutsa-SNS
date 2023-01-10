478032a2256769aeb856dc1dbdd75a3ba8ab49d1
sanghee <wjdtkdgml7352@daum.net>
2023-01-10 09:06:28 +0900

    [Update] alarm api 수정

--- src/main/java/com/first/bulletinboard/controller/AlarmController.java
+++ src/main/java/com/first/bulletinboard/controller/AlarmController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/v1/users/alarms")
+@RequestMapping("api/v1/alarms")
 @RequiredArgsConstructor
 public class AlarmController {
     private final AlarmService alarmService;
